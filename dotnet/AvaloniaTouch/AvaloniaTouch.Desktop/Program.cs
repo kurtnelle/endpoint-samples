956ac7373c8c4d07a4757f8c205211753708dc1a
Dat Tran <dat.tran@ghielectronics.com>
2024-02-12 15:30:00 -0500

    adding logo

--- dotnet/AvaloniaTouch/AvaloniaTouch.Desktop/Program.cs
+++ dotnet/AvaloniaTouch/AvaloniaTouch.Desktop/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Device.Gpio;
 using System.Device.Gpio.Drivers;
+using System.IO;
 using System.Linq;
+using System.Resources;
 using System.Threading;
 
 using Avalonia;
+using AvaloniaTouch.Desktop.Properties;
 using GHIElectronics.Endpoint.Core;
 using GHIElectronics.Endpoint.Devices.Display;
 using GHIElectronics.Endpoint.Drivers.Avalonia.Input;
 using GHIElectronics.Endpoint.Drivers.FocalTech.FT5xx6;
-using Microsoft.VisualBasic;
+
+using SkiaSharp;
 
 namespace AvaloniaTouch.Desktop;
 
@@ -21,14 +25,7 @@ class Program
     [STAThread]
     public static int Main(string[] args)
     {
-        var backlightPort = EPM815.Gpio.Pin.PD14 / 16;
-        var backlightPin = EPM815.Gpio.Pin.PD14 % 16;
-
-        var gpioDriver = new LibGpiodDriver(backlightPort);
-        var gpioController = new GpioController(PinNumberingScheme.Logical, gpioDriver);
-
-        gpioController.OpenPin(backlightPin, PinMode.Output);
-        gpioController.Write(backlightPin, PinValue.High); // low is on
+        
 
         var configuration = new FBDisplay.Configuration()
         {
@@ -46,6 +43,82 @@ class Program
         var fbDisplay = new FBDisplay(configuration);
 
         var displayController = new DisplayController(fbDisplay);
+
+        //Loading Screen
+        new Thread(() =>
+        {
+
+            var backlightPort = EPM815.Gpio.Pin.PD14 / 16;
+            var backlightPin = EPM815.Gpio.Pin.PD14 % 16;
+
+            var gpioDriver = new LibGpiodDriver(backlightPort);
+            var gpioController = new GpioController(PinNumberingScheme.Logical, gpioDriver);
+
+            gpioController.OpenPin(backlightPin, PinMode.Output);
+            gpioController.Write(backlightPin, PinValue.High); // low is on
+
+
+            SKBitmap bitmap = new SKBitmap(fbDisplay.Width, fbDisplay.Height, SKImageInfo.PlatformColorType, SKAlphaType.Premul);
+            bitmap.Erase(SKColors.Transparent);
+
+            var total_step = 6;
+            var step_length = 380 / total_step + 1;
+            
+            // Give about 7 seconds to load Avalonia UI libraries.
+            for (int i = 0; i <= total_step; i++)
+            {
+
+
+                //Initialize the SkiaSharp Canvas
+                using (var screen = new SKCanvas(bitmap))
+                {
+                    //Draw Logo
+                    var img = Resources.logo;
+                    var info = new SKImageInfo(200, 133);
+                    var sk_img = SKBitmap.Decode(img, info);
+                    screen.DrawBitmap(sk_img, 140, 20);
+
+                    //Draw Avalonia Logo
+                    var avalonlogo = Resources.avaloniaLogo;
+                    var info2 = new SKImageInfo(400, 68);
+                    var sk_imgAvaloniaLogo = SKBitmap.Decode(avalonlogo, info2);
+                    screen.DrawBitmap(sk_imgAvaloniaLogo, 30, 160);
+
+                    //Using SkiaTypeface
+                    byte[] fontfile = Resources.Myriad_Pro_Bold_Italic;
+                    var stream = new MemoryStream(fontfile);
+
+                    // Draw Line
+
+                    using (SKPaint line = new SKPaint())
+                    {
+                        line.Color = SKColors.Blue;
+                        line.IsAntialias = true;
+                        line.StrokeWidth = 20;
+                        line.Style = SKPaintStyle.Fill;
+
+                        //Rounds the ends of the line
+                        line.StrokeCap = SKStrokeCap.Round;
+
+                        if (50 + i * step_length < 430)
+                        {
+                            screen.DrawLine(50, 250, 50 + i * step_length, 250, line);
+                        }
+                        else
+                        {
+                            screen.DrawLine(50, 250, 430, 250, line);
+                        }
+                    }
+                    // Flush to screen
+                    var data = bitmap.Copy(SKColorType.Rgb565).Bytes;
+                    displayController.Flush(data);
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine($"i = {i}");
+                }
+            }
+        })
+        .Start();
 
         EPM815.I2c.Initialize(EPM815.I2c.I2c6);
 
